d35fb18021add4ab43998cc06dfd45cab988d865
HealGaren <healgaren@gmail.com>
2021-05-11 22:32:33 +0900

    알파벳 번역과 noUppercasePrefix 관련 테스트 코드 추가

---
+++ test/transpile.spec.js
@@ -0,0 +1,22 @@
+import {transpile} from "../src/transpile";
+import {expect} from "chai";
+
+describe('Transpile', () => {
+
+    it('알파벳 대문자 번역', () => {
+        const uppercaseSet = 'АӘБВГДҒЕЁЖЗИЙКҚЛМНҢОӨПРСТҮУЎФХҲЦЧШЩЪЫЬЭЮЯ';
+        expect(transpile(uppercaseSet)).to.equal('⠨⠁⠨⠜⠨⠃⠨⠺⠨⠛⠨⠙⠨⠻⠨⠑⠨⠡⠨⠚⠨⠵⠨⠊⠨⠯⠨⠅⠨⠽⠨⠇⠨⠍⠨⠝⠨⠩⠨⠕⠨⠣⠨⠏⠨⠗⠨⠎⠨⠞⠨⠌⠨⠥⠨⠧⠨⠋⠨⠓⠨⠹⠨⠉⠨⠟⠨⠱⠨⠭⠨⠷⠨⠮⠨⠾⠨⠪⠨⠳⠨⠫');
+    });
+
+    it('알파벳 소문자 번역', () => {
+        const lowercaseSet = 'аәбвгдғеёжзийкқлмнңоөпрстүуўфхҳцчшщъыьэюя'
+        expect(transpile(lowercaseSet)).to.equal('⠁⠜⠃⠺⠛⠙⠻⠑⠡⠚⠵⠊⠯⠅⠽⠇⠍⠝⠩⠕⠣⠏⠗⠎⠞⠌⠥⠧⠋⠓⠹⠉⠟⠱⠭⠷⠮⠾⠪⠳⠫');
+    })
+
+    it('noUppercasePrefix 사용 시 대문자도 소문자처럼 번역한다', () => {
+        const uppercaseSet = 'АӘБВГДҒЕЁЖЗИЙКҚЛМНҢОӨПРСТҮУЎФХҲЦЧШЩЪЫЬЭЮЯ';
+        const lowerCaseSet = 'аәбвгдғеёжзийкқлмнңоөпрстүуўфхҳцчшщъыьэюя';
+        expect(transpile(uppercaseSet, true)).to.equal(transpile(lowerCaseSet));
+    })
+
+});
